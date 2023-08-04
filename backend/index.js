a9cb247bbc3a15c8dfa400e84d5ad7aeb5401537
aisyahjefri <54765314+aisyahjefri@users.noreply.github.com>
2023-08-05 00:04:53 +0800

    Initial backend commit
    - includes CREATE items and GET ALL items function

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import mysql from "mysql";
+
+const app = express();
+
+// DB CONNECTION TO MYSQL SERVER
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "shop"
+})
+
+app.use(express.json())
+
+// DEFAULT ROUTE: LANDING PAGE
+app.get("/", (req, res)=>{
+    res.json("hello this is the backend");
+})
+
+// READ -- TO GET ALL ITEMS FROM DB & RETURN TO THIS ENDPOINT
+app.get("/items", (req, res)=>{
+    const q = "SELECT * FROM ITEMS";
+    db.query(q, (err, data)=> {
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+// CREATE -- TO ADD NEW ITEM INTO THE DB -- POST METHOD
+app.post("/items", (req, res)=>{
+    const q = "INSERT INTO ITEMS ( `title`, `description`, `cover`) VALUES (?)";
+    const values = [
+        req.body.title, req.body.description, req.body.cover
+    ];
+
+    db.query(q, [values], (err, data)=>{
+        if(err) return res.json(err);
+        return res.json("Item has been created successfully");
+    })
+})
+
+// APPLICATION RUNS ON PORT 8008
+app.listen(8008, ()=> {
+    console.log("Connected to backend!!!");
+})
