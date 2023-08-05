7b0bf9b6deb85b77ea8bb0e950f92b4856b585d2
aisyahjefri <54765314+aisyahjefri@users.noreply.github.com>
2023-08-05 16:59:46 +0800

    Adding authentication services
    - Authentication services only to pages that has update and delete functionality
    - only applies to pages for administrator
    - to add the authenticated pages at the end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,34 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import Items from "./pages/Items";
 import Add from "./pages/Add";
 import Update from "./pages/Update";
+
+// Authentication services
+import useAuth from "./services/useAuth";
+import authService from "./services/auth.service";
+
 import "./style.css"
+
+function RequireAuth({ children }) {
+  const { auth } = useAuth();
+  const location = useLocation();
+  const isLoggedIn = auth || (authService.getToken() !== null)
+
+  return isLoggedIn === true ? (
+    children
+  ) : (
+    <Navigate to="/" replace state={{ path: location.pathname }} />
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          {/* THIS ROUTE NEED AUTHENTICATION  */}
+          {/* <Route path="/" element={<RequireAuth> <Items /> </RequireAuth>} ></Route> */}
           <Route path="/" element={<Items />} ></Route>
-          {/* <Route path="/items" element={<Items />} ></Route> */}
           <Route path="/add" element={<Add />} ></Route>
           <Route path="/update/:id" element={<Update />} ></Route>
 
