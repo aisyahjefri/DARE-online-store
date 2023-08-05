221059f7714d861e11b22340d43ff3947dadcbc9
aisyahjefri <54765314+aisyahjefri@users.noreply.github.com>
2023-08-05 19:39:32 +0800

    Remove authentication services
    - to implement simple login + protected routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,35 +3,17 @@ import Items from "./pages/Items";
 import Add from "./pages/Add";
 import Update from "./pages/Update";
 
-// Authentication services
-import useAuth from "./services/useAuth";
-import authService from "./services/auth.service";
-
 import "./style.css"
-
-function RequireAuth({ children }) {
-  const { auth } = useAuth();
-  const location = useLocation();
-  const isLoggedIn = auth || (authService.getToken() !== null)
-
-  return isLoggedIn === true ? (
-    children
-  ) : (
-    <Navigate to="/" replace state={{ path: location.pathname }} />
-  );
-}
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          {/* THIS ROUTE NEED AUTHENTICATION  */}
-          {/* <Route path="/" element={<RequireAuth> <Items /> </RequireAuth>} ></Route> */}
+          
           <Route path="/" element={<Items />} ></Route>
           <Route path="/add" element={<Add />} ></Route>
           <Route path="/update/:id" element={<Update />} ></Route>
-
         </Routes>
       </BrowserRouter>
     </div>
